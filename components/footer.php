afacc1d40741ca0adff5d810a16b75b23ef8f97e
Jeffrey-Code-05 <jeffreyputter@gmail.com>
2024-10-30 10:12:45 +0200

    First Website
    Real Estate Website For My WILL Project

---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+    <div class="footer_container">
+        <div class="footer_row">
+            <div class="footer-col">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="#"><i class="fas fa-phone"></i>+27 72 180 4052</a></li>
+                    <li><a href="#"><i class="fas fa-envelope"></i>Jeffreyputter@gmail.com</a></li>
+                    <li><a href="#"><i class="fas fa-map-marker-alt"></i>krugersdorp, Silverfields, South Africa - 1739</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>General</h4>
+                <ul>
+                    <li><a href="../HTML/Home.html"><i class="fa-solid fa-house"></i>Home</a></li>
+                    <li><a href="../HTML/Search.html"><i class="fa-solid fa-magnifying-glass"></i>Search</a></li>
+                    <li><a href="../HTML/Login.html"><i class="fa-solid fa-right-to-bracket"></i>Login</a></li>
+                    <li><a href="../HTML/Register.html"><i class="fa-regular fa-registered"></i>Register</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Need Help?</h4>
+                <ul>
+                    <li><a href="../HTML/Contact_Us.html"><i class="fa-regular fa-address-book"></i>Contact Us</a></li>
+                    <li><a href="../HTML/About_Us.html"><i class="fa-solid fa-info"></i>About Us</a></li>
+                    <li><a href=".//Contact_Us.html"><i class="fa-solid fa-question"></i>FAQ</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Follow Us</h4>
+                <div class="social-links">
+                    <a href="https://www.facebook.com/jeffrey.putter.1"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://x.com/ShadownightHi"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="https://www.instagram.com/jeffreyputter/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/jeffrey-putter-ab46b431b/"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Jeffrey Putter</span> | all rights reserved!</div>
+
+</footer>
